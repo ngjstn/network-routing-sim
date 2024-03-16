4c1389e8bdf46fb4130ed261dee93e7409473398
Justin Ng <ngjustin2002@gmail.com>
2024-03-15 18:08:50 -0700

    graph topology setup works and tested

--- src/distancevector.c
+++ src/distancevector.c
@@ -35,19 +35,40 @@ typedef struct neighbour_entry
 } neighbour_entry;
 
 
+// used for creating the entry struct, not for setting the neighbour link
+static neighbour_entry* create_neighbour_entry(router* router, int id, int path_cost)
+{
+    neighbour_entry* neighbour = (neighbour_entry*)malloc(sizeof(neighbour_entry));
+    if (neighbour == NULL)
+    {
+        fprintf(stderr, "Error: Unable to allocate memory for new neighbour\n");
+        exit(1);
+    }
+    neighbour->id = id;
+    neighbour->path_cost = path_cost;
+    neighbour->router_neighbour = NULL;
+    neighbour->next = NULL;
+
+    return neighbour;
+}
+
 static void set_neighbour_link(router* router1, router* router2, int id1, int id2, int path_cost)
 {
-    // set the routers to be neighbours of each other 
-    neighbour_entry* neighbour_id1 = (neighbour_entry*)malloc(sizeof(neighbour_entry));
-    neighbour_entry* neighbour_id2 = (neighbour_entry*)malloc(sizeof(neighbour_entry));
-    neighbour_id1->id = id2;
-    neighbour_id2->id = id1;
-    neighbour_id1->path_cost = path_cost;
-    neighbour_id2->path_cost = path_cost;
-    neighbour_id1->router_neighbour = router2;
-    neighbour_id2->router_neighbour = router1;
-    router1->neighbour_list = neighbour_id1; 
-    router2->neighbour_list = neighbour_id2;
+    neighbour_entry* neighbour_id1 = create_neighbour_entry(router1, id1, path_cost);
+    neighbour_entry* neighbour_id2 = create_neighbour_entry(router2, id2, path_cost);
+    // fprintf(stdout, "router1: %d, router2: %d\n", router1->id, router2->id);
+
+    // set the neighbour entries to the front of the neighbour list
+    neighbour_entry* temp1 = router1->neighbour_list;
+    router1->neighbour_list = neighbour_id2;
+    neighbour_id2->next = temp1;
+    fprintf(stdout, "added neighbour id:%d to router:%d\n", router1->neighbour_list->id, router1->id);
+
+    neighbour_entry* temp2 = router2->neighbour_list;
+    router2->neighbour_list = neighbour_id1;
+    neighbour_id1->next = temp2;
+    fprintf(stdout, "added neighbour id:%d to router:%d\n", router2->neighbour_list->id, router2->id);
+
 }
 
 static router* get_router(int id, router* router_list)
@@ -101,26 +122,27 @@ void init_routers(router* router_list, char* topologyFile)
     }
 
     // each line in the topologyFile is a link between 2 routers
+    fprintf(stdout, "CREATING ROUTER TOPOLOGY\n");
     while ((read = getline(&line, &len, file)) != -1)
     {
-        printf("%s\n", line);
-
         char* id1 = strtok(line, " "); 
-        char* id2 = strtok(line, " ");
-        char* path_cost = strtok(line, " ");
-        printf("id1: %s, id2: %s, path_cost: %s\n", id1, id2, path_cost);
+        char* id2 = strtok(NULL, " ");
+        char* path_cost = strtok(NULL, " ");
+        printf("LINE -> id1: %s, id2: %s, path_cost: %s\n", id1, id2, path_cost);
 
         // router list is empty
         if (router_list == NULL)
         {
             // create a router for id1 and set it to router_list (head of linked list)
             router* router1 = create_router(atoi(id1)); 
+            fprintf(stdout, "created router %p, id: %d\n", router1, router1->id);
             router* router2 = create_router(atoi(id2));
+            fprintf(stdout, "created router %p, id: %d\n\n", router2, router2->id);
             set_neighbour_link(router1, router2, atoi(id1), atoi(id2), atoi(path_cost));
 
             // set router1 as the head of the list; place router2 after router1 (end of linked list)
-            router_list = router1;
-            router_list->next = router2;
+            router2->next = router1; 
+            router_list = router2;
         }
         else 
         {
@@ -130,18 +152,21 @@ void init_routers(router* router_list, char* topologyFile)
             router* router2; 
             router* current = router_list; 
             // see if router with id1 and id2 already exists
-            while (current->next != NULL)
+            while (current != NULL)
             {
+                // fprintf(stdout, "current router: %p, id: %d\n", current, current->id);
                 // found router; set local pointer and flag for reference
                 if (current->id == atoi(id1))
                 {
                     router1 = current;
                     found1 = 1;
+                    // fprintf(stdout, "found router %p, id: %d\n", router1, router1->id);
                 }
                 else if (current->id == atoi(id2))
                 {
                     router2 = current;
                     found2 = 1;
+                    // fprintf(stdout, "found router %p, id: %d\n", router2, router2->id);
                 }
 
                 // found both routers, no need to continue searching 
@@ -159,14 +184,18 @@ void init_routers(router* router_list, char* topologyFile)
                 router1 = create_router(atoi(id1));
                 router1->next = current;
                 router_list = router1;
+                fprintf(stdout, "created router %p, id: %d\n", router1, router1->id);
             }
             if (!found2)
             {
-                router1 = create_router(atoi(id2));
-                router1->next = current;
+                router2 = create_router(atoi(id2));
+                router2->next = current;
                 router_list = router2;
+                fprintf(stdout, "created router %p, id: %d\n", router2, router2->id);
             }
             
+            // fprintf(stdout, "router1: %p, router2: %p\n", router1, router2);
+            // fprintf(stdout, "router1->id: %d, router2->id: %d\n", router1->id, router2->id);
             // routers should be found or created by this point
             if (router1 == NULL || router2 == NULL)
             {
@@ -174,7 +203,26 @@ void init_routers(router* router_list, char* topologyFile)
                 exit(1);
             }
             set_neighbour_link(router1, router2, atoi(id1), atoi(id2), atoi(path_cost));
+            // fprintf(stdout, "\n");
         }
+    }
+    fclose(file);
+
+    // print out router linked list
+    fprintf(stdout, "\n\nINITIAL ROUTER LIST\n");
+    router* current = router_list;
+    while (current != NULL)
+    {
+        fprintf(stdout, "router id: %d\n", current->id);
+
+        // print out neighbour list 
+        neighbour_entry* neighbour = current->neighbour_list;
+        while (neighbour != NULL)
+        {
+            fprintf(stdout, "---> neighbour id: %d, path_cost: %d\n", neighbour->id, neighbour->path_cost);
+            neighbour = neighbour->next;
+        }
+        current = current->next;
     }
 }
 
